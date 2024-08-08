e07bb494db76ae13c7e099c7bdea20e45fc53d5e
Eric Ade ATTERE NGOUA <ericadeatterengoua@erics-mbp.mynetworksettings.com>
2024-08-08 08:11:43 -0400

    pdf to text first implementation

--- implementacion.py
+++ implementacion.py
@@ -1,2 +1,24 @@
-def addition():
-    return 25 + 35
+# import the libraries
+from pypdf import PdfReader
+# from langchain_community.document_loaders import PyPDFLoader
+
+# instanciation of the APP
+app = FastAPI()
+
+
+def pdf_to_text_01(my_pdf_file:File)->str:
+    """
+    function accepting a pdf file as input and returning a text as output
+    """
+    text = ""
+    try:
+        reader = PdfReader(my_pdf_file)
+        for page in reader.pages:
+            text += page.extract_text() + "\n"
+    except:
+        pass
+    finally:
+        return text
+
+
+
